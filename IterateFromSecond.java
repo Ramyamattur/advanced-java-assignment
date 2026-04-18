696918fef97218ac0a84bd41a07c1cc5213f1c48
Ramyamattur <vandanamattur98@gmail.com>
2026-04-18 18:40:10 +0530

    Add files via upload

---
+++ IterateFromSecond.java
@@ -0,0 +1,24 @@
+package LinkedList1;
+import java.util.*;
+
+public class IterateFromSecond {
+    public static void main(String[] args) {
+
+        // Create LinkedList
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add elements
+        list.add("Red");
+        list.add("Blue");
+        list.add("Green");
+        list.add("Yellow");
+
+        // Create iterator starting from index 1 (2nd position)
+        Iterator<String> itr = list.listIterator(1);
+
+        // Traverse elements
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
