d6a762b423af99cdc49825587a2186d3b9e9ba1c
Ramyamattur <vandanamattur98@gmail.com>
2026-04-18 18:42:05 +0530

    Add files via upload

---
+++ SwapElements.java
@@ -0,0 +1,21 @@
+package LinkedList5;
+import java.util.*;
+
+public class SwapElements {
+    public static void main(String[] args) {
+
+        // Create LinkedList
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add elements
+        list.add("Red");
+        list.add("Blue");
+        list.add("Green");
+
+        // Swap elements at index 0 and 2
+        Collections.swap(list, 0, 2);
+
+        // Display result
+        System.out.println("After swapping: " + list);
+    }
+}
