5439009ce5dc555f0a6c0766457d8a20c7a28ce7
Ramyamattur <vandanamattur98@gmail.com>
2026-04-19 20:15:52 +0530

    Add files via upload

---
+++ ColorSearch.java
@@ -0,0 +1,24 @@
+//Write a java program for getting different colors through ArrayList interface and search whether the color "Red" is available or not
+package ArrayList1;
+import java.util.*;
+
+public class ColorSearch {
+    public static void main(String[] args) {
+
+        // Create ArrayList
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Add elements
+        colors.add("Blue");
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        // Check if "Red" is present using contains()
+        if (colors.contains("Red")) {
+            System.out.println("Red color is available");
+        } else {
+            System.out.println("Red color is NOT available");
+        }
+    }
+}
