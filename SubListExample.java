33938cd36056d86b34f5c635e4001ff30fc604f0
Ramyamattur <vandanamattur98@gmail.com>
2026-04-19 20:18:07 +0530

    Add files via upload

---
+++ SubListExample.java
@@ -0,0 +1,24 @@
+//Write a java program for getting different colors through ArrayList interface and extract the elements 1st and 2nd from the ArrayList object by using SubList()
+
+package ArrayList4;
+import java.util.*;
+
+public class SubListExample {
+    public static void main(String[] args) {
+
+        // Create ArrayList
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Add elements
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        // Extract elements from index 0 to 1 (end index is exclusive)
+        List<String> subList = colors.subList(0, 2);
+
+        // Display extracted list
+        System.out.println("Extracted elements: " + subList);
+    }
+}
