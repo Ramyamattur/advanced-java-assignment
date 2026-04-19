3137de68ab348b0216aa7fcee37485d1c6bcb2b9
Ramyamattur <vandanamattur98@gmail.com>
2026-04-19 20:37:45 +0530

    Add files via upload

---
+++ CountryListDemo.java
@@ -0,0 +1,55 @@
+//5a. Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada, Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and display them on console whenever the countries are selected on the list.
+
+package swingdemo;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+
+public class CountryListDemo {
+
+    public static void main(String[] args) {
+
+        // Create Frame
+        JFrame frame = new JFrame("Country List");
+
+        // List of Countries
+        String[] countries = {
+            "USA", "India", "Vietnam", "Canada", "Denmark",
+            "France", "Great Britain", "Japan", "Africa",
+            "Greenland", "Singapore"
+        };
+
+        // Create JList
+        JList<String> countryList = new JList<>(countries);
+
+        // Set selection mode (single selection)
+        countryList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        // Add Scroll Pane
+        JScrollPane scrollPane = new JScrollPane(countryList);
+
+        // Add ListSelectionListener (Event Handling)
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+
+                // Avoid duplicate events
+                if (!e.getValueIsAdjusting()) {
+                    String selected = countryList.getSelectedValue();
+
+                    if (selected != null) {
+                        System.out.println("Selected Country: " + selected);
+                    }
+                }
+            }
+        });
+
+        // Add components to frame
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        // Frame settings
+        frame.setSize(300, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
