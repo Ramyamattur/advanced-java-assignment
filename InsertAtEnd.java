1377c0f7a8000e5e8f65bbd2a1b8f26108a13196
Ramyamattur <vandanamattur98@gmail.com>
2026-04-19 20:20:16 +0530

    Add files via upload

---
+++ InsertAtEnd.java
@@ -0,0 +1,21 @@
+//Write a Java program to insert the specified element at the end of a linked list.( using l_listobj.offerLast("Pink"))
+package LinkedList3;
+import java.util.*;
+
+public class InsertAtEnd {
+    public static void main(String[] args) {
+
+        // Create LinkedList
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add elements
+        list.add("Red");
+        list.add("Blue");
+
+        // Insert element at the end using offerLast()
+        list.offerLast("Pink");
+
+        // Display list
+        System.out.println("After inserting at end: " + list);
+    }
+}
